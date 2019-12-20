5517fd31dcc07cfeff55d250d42a547b7cfa1ec7
wuxiao <1291383707@qq.com>
2019-12-20 20:45:15 +0800

    表单样式完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,30 +1,73 @@
 <template>
-    <div class="loginWrapper">
-        <div class="loginBox">
-            <div class="avaterBox">
-                <img src="" alt="">
-            </div>
-        </div>
+  <div class="loginWrapper">
+    <div class="loginBox">
+      <div class="avaterBox">
+        <img src="../assets/logo.png" alt="picture" />
+      </div>
+      <el-form label-width="0" class="loginForm">
+        <el-form-item>
+            <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-input prefix-icon="iconfont icon-password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
-export default {}
+
 </script>
 
 <style lang="less" scoped>
-    .loginWrapper {
-        height: 100%;
-        background-color: cadetblue;
+.loginWrapper {
+  height: 100%;
+  background-color: cadetblue;
+}
+.loginBox {
+  position: absolute;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 0 10px 0 rgba(0, 0, 2, 0.5);
+  .avaterBox {
+    width: 135px;
+    height: 135px;
+    border: 1px solid grey;
+    border-radius: 50%;
+    padding: 10px;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0 0 10px 0 rgba(0, 0, 2, 0.5);
+    img {
+      width: 100%;
+      height: 100%;
+      border: 1px solid grey;
+      background-color: #eee;
+      border-radius: 50%;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 2, 0.5);
     }
-    .loginWrapper .loginBox {
+  }
+}
+    .loginForm {
         position: absolute;
-        width: 450px;
-        height: 300px;
-        background-color: #fff;
-        border-radius: 3px;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
+        bottom: 0;
+        padding: 0 20px;
+        width: 100%;
+        .btns{
+            display: flex;
+            justify-content: flex-end;
+        }
     }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, form, formItem, input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(form)
+Vue.use(formItem)
+Vue.use(input)
