9d70c17fe3ba78250d0b3a4a82754dabf9f0f768
wuxiao <1291383707@qq.com>
2019-12-20 11:58:37 +0800

    添加登陆界面样式01

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        登录组件
+    <div class="loginWrapper">
+        <div class="loginBox">
+            <div class="avaterBox">
+                <img src="" alt="">
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
-
+    .loginWrapper {
+        height: 100%;
+        background-color: cadetblue;
+    }
+    .loginWrapper .loginBox {
+        position: absolute;
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 3px;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
