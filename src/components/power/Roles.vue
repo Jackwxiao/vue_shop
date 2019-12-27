6100f8ad01024bc5cbcf901f96d95ca72cc8c81d
wuxiao <1291383707@qq.com>
2019-12-27 12:16:57 +0800

    添加角色对话框UI以及数据请求完成

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -8,45 +8,94 @@
     <el-card>
       <el-row>
         <el-col>
-          <el-button type="primary">添加角色</el-button>
+          <el-button type="primary" @click="addDialog">添加角色</el-button>
         </el-col>
       </el-row>
       <el-table :data="rolesList" border stripe>
-          <el-table-column type="expand"></el-table-column>
-          <el-table-column type="index"></el-table-column>
-          <el-table-column label="角色名称" prop="roleName"></el-table-column>
-          <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
-          <el-table-column label="操作" width="300px">
-              <template>
-                  <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
-                  <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
-                  <el-button size="mini" type="warning" icon="el-icon-setting">分配角色</el-button>
-              </template>
-          </el-table-column>
+        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template>
+            <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting">分配角色</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
+    <!-- 添加角色对话框 -->
+    <el-dialog title="添加角色" :visible.sync="dialogVisible" width="60%" @close="closeResetDialog">
+      <el-form
+        :model="addRolesForm"
+        :rules="addRolesFormRules"
+        ref="addRolesFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="addRolesForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="addRolesForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addRoles">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            rolesList: []
-        }
-    },
-    created(){
-        this.getRolesList()
-    },
-    methods: {
-        async getRolesList() {
-          const { data: res } = await this.$http.get('roles')
-          if (res.meta.status !== 200) {
-              return this.$message.error('获取角色列表失败！')
-          }
-          this.rolesList = res.data
-        }
+  data() {
+    return {
+      rolesList: [],
+      dialogVisible: false,
+      addRolesForm: {
+        roleName: '',
+        roleDesc: ''
+      },
+      addRolesFormRules: {
+        roleName: [
+          { required: true, message: '请输入角色名', trigger: 'blur' }
+        ],
+        roleDesc: [{ required: true, message: '请输入角色名', trigger: 'blur' }]
+      }
     }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败！')
+      }
+      this.rolesList = res.data
+    },
+    addDialog() {
+      this.dialogVisible = !this.dialogVisible
+    },
+    closeResetDialog() {
+      this.$refs.addRolesFormRef.resetFields()
+    },
+    addRoles() {
+      this.$refs.addRolesFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('roles', this.addRolesForm)
+        if (res.meta.status !== 201) {
+          this.$message.error('角色添加失败！')
+        }
+        this.dialogVisible = false
+        this.getRolesList()
+        this.$message.success('角色添加成功！')
+      })
+    }
+  }
 }
 </script>
 
