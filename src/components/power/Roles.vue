c09e22804209817bbd4a60874edf2a327a5b2fdd
wuxiao <1291383707@qq.com>
2019-12-28 11:02:37 +0800

    下拉详细信息结构完成

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -12,14 +12,54 @@
         </el-col>
       </el-row>
       <el-table :data="rolesList" border stripe>
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['borderBottom', i1  === 0 ? 'borderTop' : '']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
+              <el-col :span="5">
+                <el-tag type="success">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <el-row
+                  :class="[i2 === 0 ? '': 'borderTop']"
+                  v-for="(item2, i2) in item1.children"
+                  :key="item2.id"
+                >
+                  <el-col :span="6">
+                    <el-tag>{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                      <el-tag type="warning" v-for="(item3) in item2.children" :key="item3.id">
+                          {{item3.authName}}
+                      </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
         <el-table-column label="操作" width="300px">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" icon="el-icon-edit" @click="expandEditDialog(scope.row.id)">编辑</el-button>
-            <el-button size="mini" type="danger" icon="el-icon-delete" @click="removeRoleByRoleId(scope.row.id)">删除</el-button>
+            <el-button
+              size="mini"
+              type="primary"
+              icon="el-icon-edit"
+              @click="expandEditDialog(scope.row.id)"
+            >编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+              @click="removeRoleByRoleId(scope.row.id)"
+            >删除</el-button>
             <el-button size="mini" type="warning" icon="el-icon-setting">分配角色</el-button>
           </template>
         </el-table-column>
@@ -46,8 +86,18 @@
       </span>
     </el-dialog>
     <!-- 修改角色对话框 -->
-    <el-dialog title="修改角色" :visible.sync="editRolesDialogVisible" width="50%" @close="cancelResetDialog">
-      <el-form :model="editRolesForm" :rules="addRolesFormRules" ref="editRolesFormRef" label-width="80px">
+    <el-dialog
+      title="修改角色"
+      :visible.sync="editRolesDialogVisible"
+      width="50%"
+      @close="cancelResetDialog"
+    >
+      <el-form
+        :model="editRolesForm"
+        :rules="addRolesFormRules"
+        ref="editRolesFormRef"
+        label-width="80px"
+      >
         <el-form-item label="角色名称" prop="roleName">
           <el-input v-model="editRolesForm.roleName"></el-input>
         </el-form-item>
@@ -113,53 +163,66 @@ export default {
       })
     },
     async expandEditDialog(id) {
-        const { data: res } = await this.$http.get('roles/' + id)
-        if (res.meta.status !== 200) {
-            return this.$message.error('查询角色信息失败')
-        }
-        this.editRolesForm = res.data
-        this.editRolesDialogVisible = true
+      const { data: res } = await this.$http.get('roles/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('查询角色信息失败')
+      }
+      this.editRolesForm = res.data
+      this.editRolesDialogVisible = true
     },
     cancelResetDialog() {
-        this.$refs.editRolesFormRef.resetFields()
+      this.$refs.editRolesFormRef.resetFields()
     },
     editRolesInfo() {
-        this.$refs.editRolesFormRef.validate(async valid => {
-            if (!valid) return
-            const { data: res } = await this.$http.put('roles/' + this.editRolesForm.roleId, {
-                roleName: this.editRolesForm.roleName,
-                roleDesc: this.editRolesForm.roleDesc
-        })
+      this.$refs.editRolesFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.put(
+          'roles/' + this.editRolesForm.roleId,
+          {
+            roleName: this.editRolesForm.roleName,
+            roleDesc: this.editRolesForm.roleDesc
+          }
+        )
         if (res.meta.status !== 200) {
-            this.$message.error('更新失败！')
+          this.$message.error('更新失败！')
         }
         this.editRolesDialogVisible = false
         this.getRolesList()
         this.$message.success('更新成功！')
-  })
+      })
     },
     async removeRoleByRoleId(id) {
-        const confirmResult = await this.$confirm('将永久删除该用户，是否继续操作？',
+      const confirmResult = await this.$confirm(
+        '将永久删除该用户，是否继续操作？',
         '温馨提示',
         {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
         }
-        ).catch(err => err)
-        if (confirmResult !== 'confirm') {
-            return this.$message.info('取消删除')
-        }
-        const { data: res } = await this.$http.delete('roles/' + id)
-        if (res.meta.status !== 200) {
-            return this.$message.error('删除角色失败！')
-        }
-        this.$message.success('删除成功！')
-        this.getRolesList()
+      ).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消删除')
+      }
+      const { data: res } = await this.$http.delete('roles/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除角色失败！')
+      }
+      this.$message.success('删除成功！')
+      this.getRolesList()
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.el-tag {
+  margin: 8px;
+}
+.borderTop {
+  border-top: 1px solid #eee;
+}
+.borderBottom {
+  border-bottom: 1px solid #eee;
+}
 </style>
