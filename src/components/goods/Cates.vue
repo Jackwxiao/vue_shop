c056b4180844cd4a4d39f637e75cef2a6eb17b48
wuxiao <1291383707@qq.com>
2019-12-31 19:43:55 +0800

    商品分类页数据获取

--- src/components/goods/Cates.vue
+++ src/components/goods/Cates.vue
@@ -6,19 +6,41 @@
       <el-breadcrumb-item>商品列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-        <el-row>
-            <el-button type="primary">添加分类</el-button>
-        </el-row>
+      <el-row>
+        <el-button type="primary">添加分类</el-button>
+      </el-row>
     </el-card>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+        querInfo: {
+           type: 3,
+           pagenum: 1,
+           pagesize: 5
+        },
+        cateList: [],
+        total: 0
+    }
   },
-  created() {},
-  methods() {}
+  created() {
+      // 获取商品分裂数据
+      this.getCateList()
+  },
+  methods: {
+     async getCateList() {
+        const { data: res } = await this.$http.get('categories', {
+            params: this.querInfo })
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取商品列表失败！')
+            }
+            console.log(res.data)
+            this.cateList = res.data.result// 数据列表
+            this.total = res.data.total// 总数据条数
+  }
+}
 }
 </script>
 <style lang="less" scoped>
