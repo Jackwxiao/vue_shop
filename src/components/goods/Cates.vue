fb6a436a777a2125ad5659768146e0caa3eb0d16
wuxiao <1291383707@qq.com>
2019-12-31 19:16:18 +0800

    商品分类页面结构初始化

--- src/components/goods/Cates.vue
+++ src/components/goods/Cates.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>商品分类组件</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+        <el-row>
+            <el-button type="primary">添加分类</el-button>
+        </el-row>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-        return {}
-    },
-    created () {},
-    methods () {}
+  data() {
+    return {}
+  },
+  created() {},
+  methods() {}
 }
 </script>
 <style lang="less" scoped>
-
 </style>
