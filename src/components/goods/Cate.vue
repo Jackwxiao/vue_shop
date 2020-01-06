f887b7c32bbe3e7527c516abfa5191f284145a7b
wuxiao <1291383707@qq.com>
2020-01-06 22:42:23 +0800

    引入vue-table-with-tree-grid

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -6,17 +6,53 @@
       <el-breadcrumb-item>商品分类</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-        <el-row>
-            <el-col>
-                <el-button type="primary">添加分类</el-button>
-            </el-col>
-        </el-row>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
     </el-card>
-    
+    <tree-table border index-text="#" show-index :expand-type="false" :selection-type="false" show-row-hover :data="cateList" :columns="columns"></tree-table>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类数据列表
+      cateList: [],
+      total: 0,
+      columns: [
+        {
+          label: '类名',
+          prop: 'cat_name'
+        }
+      ]
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败！')
+      }
+      console.log(res.data)
+      this.cateList = res.data.result
+      this.total = res.data.total
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 </style>
