c0f1b50c5e1565436d899aaa25d73c2e78f76547
wuxiao <1291383707@qq.com>
2020-01-14 14:29:08 +0800

    v-model实现搜索功能

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -8,66 +8,82 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getGoodsList">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-            <el-button type="primary">添加商品</el-button>
+          <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
       <el-table :data="goodslist" border stripe>
-           <el-table-column type="index"></el-table-column>
-           <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-           <el-table-column label="商品价格(元)" prop="goods_price" width="105px"></el-table-column>
-           <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
-           <el-table-column label="创建时间" prop="add_time" width="170px">
-               <template scope="scope">
-                   {{scope.row.add_time | dateFormat}}
-               </template>
-           </el-table-column>
-           <el-table-column label="操作" width="130px">
-               <template>
-                   <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-                   <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-               </template>
-           </el-table-column>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="105px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="170px">
+          <template scope="scope">{{scope.row.add_time | dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 20, 30]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      ></el-pagination>
     </el-card>
   </div>
 </template>
 <script>
 export default {
   data() {
-      return {
-          // 查询参数列表
-          queryInfo: {
-              query: '',
-              pagenum: 1,
-              pagesize: 10
-          },
-          // 商品列表
-          goodslist: [],
-          // 商品总数
-          total: 0
-      }
+    return {
+      // 查询参数列表
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 商品列表
+      goodslist: [],
+      // 商品总数
+      total: 0
+    }
   },
   created() {
-      this.getGoodsList()
+    this.getGoodsList()
   },
   methods: {
-     async getGoodsList() {
-         const { data: res } = await this.$http.get('goods', {
-              params: this.queryInfo
-          })
-          if (res.meta.status !== 200) {
-              return this.$message.error('获取商品列表失败！')
-          }
-          this.$message.success('获取商品列表成功！')
-          console.log(res.data)
-          this.goodslist = res.data.goods
-          this.total = res.data.total
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败！')
       }
+      this.$message.success('获取商品列表成功！')
+      console.log(res.data)
+      this.goodslist = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    }
   }
 }
 </script>
