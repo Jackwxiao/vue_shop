f44d686714783e701084dfe9fc0046536e5f1bcb
wuxiao <1291383707@qq.com>
2020-01-14 14:11:13 +0800

    设置全局时间过滤器，自定义时间

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -19,9 +19,13 @@
       <el-table :data="goodslist" border stripe>
            <el-table-column type="index"></el-table-column>
            <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-           <el-table-column label="商品价格（元）" prop="goods_price" width="95px"></el-table-column>
-           <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
-           <el-table-column label="创建时间" prop="add_time" width="140px"></el-table-column>
+           <el-table-column label="商品价格(元)" prop="goods_price" width="105px"></el-table-column>
+           <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
+           <el-table-column label="创建时间" prop="add_time" width="170px">
+               <template scope="scope">
+                   {{scope.row.add_time | dateFormat}}
+               </template>
+           </el-table-column>
            <el-table-column label="操作" width="130px">
                <template>
                    <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
--- src/main.js
+++ src/main.js
@@ -18,6 +18,17 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0') // 月份从 0 开始，所以+1 ,+ '' 是变成字符串
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}.`
+})
+
 new Vue({
   router,
   render: h => h(App)
