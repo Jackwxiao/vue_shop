edda0abd03782d324c4dd017c9503e99021cbdfb
wuxiao <1291383707@qq.com>
2020-01-18 15:31:59 +0800

    修鞋模块未声明

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/css/icon.css'
-
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 import ZkTable from 'vue-table-with-tree-grid'
-Vue.component('tree-table', ZkTable)
-
 import axios from 'axios'
+
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   console.log(config)
@@ -17,6 +19,8 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.component('tree-table', ZkTable)
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
